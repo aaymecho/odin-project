fa6e907ee1b1b29ef63259ca9960b055a1526760
aaymecho <aaymecho3@gatech.edu>
2023-06-09 10:17:41 -0400

    added ui for rps

--- work/rps.js
+++ work/rps.js
@@ -1,6 +1,13 @@
 let choices = ['rock', 'paper', 'scissor']
 let compScore = 0;
 let playerScore = 0;
+const button = document.querySelectorAll('button')
+
+//Section for showing results
+const content = document.querySelector('#result')
+const result = document.createElement('h1')
+
+
 
 function getComputerChoice() {
   return Math.floor(Math.random() * 3);
@@ -11,22 +18,38 @@ function playRound(playerSelection, computerSelection) {
   const playerMove = choices.indexOf(playerSelection.toLowerCase());
 
   if (playerMove == computerSelection)
-    return "It is a tie!";
+    console.log("It's a tie!")
 
   else if ((playerMove == 0 && computerSelection == 2) ||
     (playerMove == 1 && computerSelection == 0) ||
     (playerMove == 2 && computerSelection == 1)) {
-    playerScore++;
-    return "You Win!";
+    ++playerScore
+
+    console.log("You win!")
   }
   else {
-    compScore++;
-    return "You lose!";
+    ++compScore
+    console.log("You lose!")
+  }
+
+
+  if (compScore == 5) {
+    result.textContent = 'The computer has won!'
+    compScore = 0, playerScore = 0
+  }
+  else if (playerScore == 5) {
+    result.textContent = 'You have won!'
+    compScore = 0, playerScore = 0
+  } else {
+    result.textContent = `Wins:${playerScore} Loss:${compScore}`
   }
 }
 
-function game(loop) {
-  compScore = 0;
-  playerScore = 0;
-  console.log(playRound(prompt("What is your choice?"), getComputerChoice()));
-}
+button.forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    playRound(e.target.outerText, getComputerChoice())
+  })
+})
+
+content.appendChild(result)
+
